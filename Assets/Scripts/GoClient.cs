84a992ab8f545c3ddcadd4c73572d4fb48d22123
MaungOo <83910863+maungoo112358@users.noreply.github.com>
2025-06-16 14:16:16 +0700

    Initial commit

---
+++ Assets/Scripts/GoClient.cs
@@ -0,0 +1,41 @@
+using Gamepacket;
+using Google.Protobuf;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class GoClient : MonoBehaviour
+{
+	private UdpClient _client;
+	private IPEndPoint _remoteEndPoint;
+
+	private void Start()
+	{
+		_client = new UdpClient();
+		_remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 9999);
+
+		var handshake = new GamePacket
+		{
+			HandshakeRequest = new HandshakeRequest { ClientName = "Slint" }
+		};
+		byte[] data = handshake.ToByteArray();
+		_client.Send(data, data.Length, _remoteEndPoint);
+	}
+
+	private void SendMessage(string message, uint seq)
+	{
+		var packet = new GamePacket()
+		{
+			Seq = seq,
+			ChatMessage = new ChatMessage
+			{
+				Message = message,
+			}
+		};
+
+		byte[] data = MessageExtensions.ToByteArray(packet);
+
+		_client.Send(data, data.Length, _remoteEndPoint);
+		Debug.Log($"Send packet to go server=> SeqID: {seq} <:::> Message: {message}");
+	}
+}
